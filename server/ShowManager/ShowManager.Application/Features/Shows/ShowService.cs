e6c4bc4b6ebda9ee0b5ded9631988408849a9d1e
johnBoeira <johnboeira@gmail.com>
2025-03-22 12:04:27 -0300

    feat: Adiciona camada de application

---
+++ server/ShowManager/ShowManager.Application/Features/Shows/ShowService.cs
@@ -0,0 +1,62 @@
+﻿using ShowManager.Dominio.DTO;
+using ShowManager.Dominio.Features.Shows;
+using ShowManager.Infra.DataBase.Repository.Shows;
+using ShowManager.Infra.DataBase.Repository.Usuarios;
+
+namespace ShowManager.Aplicacao.Features.Shows
+{
+    public class ShowService : IShowService
+    {
+
+        private readonly ShowRepository showRepository ;
+
+        public async Task<Show> Atualizar(ShowEditarDTO showEditarDTO, int id)
+        {
+            var show = new Show
+            {
+                Id = id,
+                NomeShow = showEditarDTO.NomeShow,
+                DataInicio = showEditarDTO.DataInicio,
+                DataFim = showEditarDTO.DataFim,
+                NumeroParticipantes = showEditarDTO.NumeroParticipantes,
+                Duracao = showEditarDTO.Duracao,
+                OrganizadorId = showEditarDTO.OrganizadorId,
+                Organizador = showEditarDTO.Organizador
+            };
+            return await showRepository.SaveAsync(show);
+           
+        }
+
+        public async Task<IEnumerable<Show>?> Buscar()
+        {
+            return await showRepository.GetAllAsync();
+        }
+
+        public Task<Show?> BuscarPorID(int id)
+        {
+            return showRepository.GetByIdAsync(id);
+        }
+
+        public async Task<Show> Criar(ShowAdicionarDTO showAdicionarDTO)
+        {
+            var show = new Show
+            {
+                NomeShow = showAdicionarDTO.NomeShow,
+                DataInicio = showAdicionarDTO.DataInicio,
+                DataFim = showAdicionarDTO.DataFim,
+                NumeroParticipantes = showAdicionarDTO.NumeroParticipantes,
+                Duracao = showAdicionarDTO.Duracao,
+                OrganizadorId = showAdicionarDTO.OrganizadorId,
+                Organizador = showAdicionarDTO.Organizador
+            };
+            return await showRepository.SaveAsync(show);
+        }
+
+        public async Task<int> Deletar(int id)
+        {
+
+            await showRepository.DeleteAsync(id);
+            return id;
+        }
+    }
+}
---
+++ server/ShowManager/ShowManager.Application/Features/Usuarios/UsuarioService.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualBasic;
+using ShowManager.Dominio.DTO;
+using ShowManager.Dominio.Features.Usuarios;
+using ShowManager.Infra.DataBase.Repository.Usuarios;
+
+namespace ShowManager.Aplicacao.Features.Usuarios;
+
+public class UsuarioService : IUsuarioService
+{
+    private readonly UsuarioRepository _usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository)
+    {
+        this._usuarioRepository = usuarioRepository;
+    }
+
+    public async Task<Usuario> Atualizar(UsuarioEditarDTO usuarioEditarDTO, int id)
+    {
+
+        var usuario = new Usuario
+        {
+            Id = id,
+            Nome = usuarioEditarDTO.Nome,
+            Email = usuarioEditarDTO.Email,
+            Senha = usuarioEditarDTO.Senha,
+            TipoUsuarioEnum = usuarioEditarDTO.TipoUsuarioEnum
+        };
+        var usuarioAtualizado = await _usuarioRepository.SaveAsync(usuario);
+        return usuarioAtualizado;
+    }
+
+    public async Task<IEnumerable<Usuario>?> Buscar()
+    {
+        return await _usuarioRepository.GetAllAsync();
+    }
+
+    public async Task<Usuario?> BuscarPorID(int id)
+    {
+        return await _usuarioRepository.GetByIdAsync(id);
+    }
+
+    public async Task<Usuario> Criar(UsuarioAdicionarDTO usuarioAdicionarDTO)
+    {
+        var usuario = new Usuario
+        {
+            Nome = usuarioAdicionarDTO.Nome,
+            Email = usuarioAdicionarDTO.Email,
+            Senha = usuarioAdicionarDTO.Senha,
+            TipoUsuarioEnum = usuarioAdicionarDTO.TipoUsuarioEnum
+        };
+        return await _usuarioRepository.SaveAsync(usuario);
+    }
+
+    public async Task<int> Deletar(int id)
+    {
+        await _usuarioRepository.DeleteAsync(id);
+        return id;
+    }
+}
